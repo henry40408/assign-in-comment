9e0db054b6fdc395957941415375f756bacbfa20
Heng-Yi Wu <henry40408@gmail.com>
2018-02-13 17:49:10 +0800

    feat: Display booting message

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const { assignInComment } = require('./lib')
+const { log, assignInComment } = require('./lib')
 
 module.exports = robot => {
+  log(robot, 'assign-in-comment bot is on!')
   robot.on('pull_request.opened', context => assignInComment(robot, context))
   robot.on('pull_request.edited', context => assignInComment(robot, context))
 }
