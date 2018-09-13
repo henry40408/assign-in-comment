965072fd57b188479a4c0263c541a055296dc7bf
Heng-Yi Wu <henry40408@gmail.com>
2018-09-13 16:22:10 +0800

    refactor: assignInComment function 改名

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { log, assignInComment } = require('./lib')
+const { log, assignInPullRequest } = require('./lib')
 
 module.exports = robot => {
   log(robot, 'assign-in-comment bot is on!')
-  robot.on('pull_request.opened', context => assignInComment(robot, context))
-  robot.on('pull_request.edited', context => assignInComment(robot, context))
+  robot.on('pull_request.opened', context => assignInPullRequest(robot, context))
+  robot.on('pull_request.edited', context => assignInPullRequest(robot, context))
 }
